5f804f56d0df092681eedd16306640bd198bab89
augusto-cedaro <cedaroaugusto@gmail.com>
2022-10-04 17:23:45 -0300

    Atualizações

--- index.php
+++ index.php
@@ -10,15 +10,65 @@
 </head>
 <body>
     <header>
-            <nav class="navbar bg-dark container-fluid">
+            <nav class="navbar bg-dark container-fluid navbar-expand-lg d-flex justify-content-between">
                 <a href="#" class="navbar-brand text-white">Fabiano Toná</a>
+                <div class="d-inline-block">
+                    <ul class="navbar-nav p-3">
+                        <li class="nav-item h4">
+                            <a href="#" class="nav-link text-white">Home</a>
+                        </li>
+                        <li class="nav-item h4">
+                            <a href="#" class="nav-link text-white">Curriculo</a>
+                        </li>
+                        <li class="nav-item h4">
+                            <a href="#" class="nav-link text-white">Contato</a>
+                        </li>
+                    </ul>
+                </div>   
             </nav>
     </header>
 
     <section>
+        <div class="container-fluid row">
+            <div class="col-6  d-flex justify-content-center pt-5 pb-5">
+                <img src="images/man.jpg" alt="palestrante" class="w-50 border border-dark border-4">
+            </div>
+            <div class="col-6 pt-5 pb-5">
+                <p class="h2 d-flex justify-content-center">Apresentação</p><br>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+                     printer took a galley of type and scrambled it to make a type specimen book. 
+                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+                     printer took a galley of type and scrambled it to make a type specimen book. 
+                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+        </div>
     </section>
 
     <section>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="..." class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="...">
+            </div>
+         </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+    </div>
     </section>
 
     <section>
